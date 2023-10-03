969ab0522aeeac464e9eacb07f8351b36c4ebff4
Shane <hussey.shane6@gmail.com>
2023-10-03 19:18:33 -0400

    Included Randomized Quicksort
    Included Randomized Quicksort

--- quicksort.cpp
+++ quicksort.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include <cstdlib>
 #include<vector>
 #include<cmath>
 using namespace std;
@@ -29,11 +30,41 @@ void quicksort(vector<int>& arr, int start, int end) {
 }
 
 
+int random_partition(vector<int>& arr, int start, int end) {
+    
+    
+    int p = rand() % (end + 1);
+    cout << p << "\n";
+    int pivot = arr[start];
+    int i = start;
+    for (int j = start + 1; j <= end ;j++){
+        if (arr[j] <= pivot) {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[start], arr[i]);
+    return i;
+    }
+
+
+
+void random_quicksort(vector<int>& arr, int start, int end) {
+    if (start < end) {
+        int midpoint = random_partition(arr, start, end);
+        random_quicksort(arr, start, midpoint - 1);
+        random_quicksort(arr, midpoint + 1, end);
+    }
+}
+
+
+
 int main() {
+    srand (time(0));
     vector<int> arr = {1,3,8,2,5,4,9,6};
-    quicksort(arr ,0,arr.size() - 1);
+    random_quicksort(arr ,0,arr.size() - 1);
     for (int x = 0; x < arr.size(); x++) {
-        cout << " "<< arr[x];
+        cout << " " << arr[x];
     }
     cout << "\n";
     return 0;
