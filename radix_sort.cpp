69e6f2c8dd6ec669499fee32589df5b6a5188eb4
Shane <hussey.shane6@gmail.com>
2023-10-14 11:45:12 -0400

    Corrected Radix Sort - Complete

--- radix_sort.cpp
+++ radix_sort.cpp
@@ -1,66 +1,60 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-
-
-vector<int> counting_sort (vector<int> A, int d) {
-    // create Vector B of A.length()
-    vector<int> B(A.size(), 0);
-
-    // k will always be equal to 10 for counting sort
+vector<int> counting_sort(vector<int> A, int d) {
+    // k is always equal to 10 for counting sort
     const int k = 10;
     
-    // let C be a new array of length k + 1 (for 0), with each element being a zero
-    vector<int> C(10, 0);
+    // create vectors B and C 
+    vector<int> B(A.size(), 0);
+    vector<int> C(k, 0);
 
-    // Assign C indecies the count of the number 
+    // assign C indices the count of each number 
     for (int j = 0; j < A.size(); j++) {
-        C[A[j]] = C[A[j]] + 1;
+        int digit = (A[j] / d) % 10;
+        C[digit]++;
     }
     
-    for (int i = 1; i <= k; i++) {
-        C[i] = C[i] + C[i - 1];
-    }
-    // output counts in C to B
-    for (int j = A.size() - 1; j >= 0; j-- ) {
-        B[C[A[j]] - 1] = A[j];
-        C[A[j]] = C[A[j]] - 1;
+    for (int i = 1; i < k; i++) {
+        C[i] += C[i - 1];
     }
 
-    // return our sorted array C
+    // output counts in C to B
+    for (int j = A.size() - 1; j >= 0; j--) {
+        int digit = (A[j] / d) % 10;
+        B[C[digit] - 1] = A[j];
+        C[digit]--;
+    }
+
+    // return the sorted array B
     return B;
 }
 
-
-
-vector<int> radix_sort(vector<int> A, int d) {
-    for (i =0; i < d; i++) {
-
+vector<int> radix_sort(vector<int> A) {
+    // find the max element to determine the number of digits
+    vector<int>::iterator maxElementIterator = max_element(A.begin(), A.end());
+    int maxElement = *maxElementIterator;
+    
+    // Perform counting sort for each digit position
+    for (int d = 1; maxElement / d > 0; d *= 10) {
+        A = counting_sort(A, d);
     }
+    
+    return A;
 }
 
-
 int main() {
-    // define our input vector
-    vector<int> A = {999, 621, 877, 358, 534, 723, 423, 365, 443, 223, 153};
+    // Define the input vector
+    vector<int> A =  {329, 457, 657, 839, 436, 720, 353};
 
-    // create an vector B with A.size() elements filled with 0s
-    vector<int> B(A.size(), 0);
+    // Call radix sort on A
+    vector<int> sortedArray = radix_sort(A);
 
-    // determine the highest number k for counting sort
-    vector<int>::iterator maxElementIterator = max_element(A.begin(), A.end());
-    int k = *maxElementIterator;
-    cout << k << "\n";
-
-
-    // call counting sort on A
-    vector<int> C = counting_sort(A, B, k);
-
-    // print out sorted array
-    for (int i = 0; i < A.size(); i++) {
-        cout << C[i] << " ";
+    // Print out the sorted array
+    for (int i = 0; i < sortedArray.size(); i++) {
+        cout << sortedArray[i] << " ";
     }
     cout << "\n";
 
